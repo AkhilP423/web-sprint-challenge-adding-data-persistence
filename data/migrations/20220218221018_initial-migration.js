4324a8aa378e3846ad95cd8ebc7692f2f0c9fec0
Akhil P <akhilpinnani96@gmail.com>
2022-02-18 15:49:57 -0800

    reverted migration file

--- data/migrations/20220218221018_initial-migration.js
+++ data/migrations/20220218221018_initial-migration.js
@@ -1,69 +1,66 @@
-//retried with async, should work
-exports.up = async function(knex) {
+exports.up = function(knex, Promise){
+    return knex.schema
+    .createTable('projects', table => {
 
-    await knex.schema
+        table.increments('project_id')
+        table.string('project_name', 128)
+        .notNullable()
+        table.string('project_description', 128)
+        table.integer('project_completed')
+        .defaultTo(0)
+        .unsigned()
 
-        .createTable('projects', (table) => {
-            table.increments('project_id')
-            table.string('project_name', 128)
-                .notNullable()
-            table.string('project_description', 255)
-            table.boolean('project_completed')
-                .defaultTo(false)
-        })
+    })
 
-        .createTable('resources', (table) => {
-            table.increments('resource_id')
-            table.string('resource_name', 128)
-                .notNullable()
-                .unique()
-            table.string('resource_description', 255)
-        })
+    .createTable('resources', table => {
 
-        .createTable('tasks', (table) => {
+        table.increments('resource_id')
+        table.string('resource_name', 128)
+        .notNullable()
+        .unique()
+        table.string('resource_description', 128)
 
-            table.increments('task_id')
-            table.string('task_description', 255)
-                .notNullable()
-            table.string('task_notes', 255)
-            table.boolean('task_completed')
-                .defaultTo(false)
-            table.integer('project_id')
-                .unsigned()
-                .notNullable()
-                .references('project_id')
-                .inTable('projects')
-                .onDelete('RESTRICT')
-                .onUpdate('RESTRICT')
+    })
 
-        })
+    .createTable('tasks', table => {
 
-        .createTable('project_resources', (table) => {
+        table.increments('task_id')
+        table.string('task_description', 128)
+        .notNullable()
+        table.string('task_notes')
+        table.integer('task_completed')
+        .defaultTo(0)
+        .unsigned()
+        table.integer('project_id')
+        .notNullable()
+        .unsigned()
+        .references('project_id')
+        .inTable('projects')
+        .onUpdate('RESTRICT')
+        .onDelete('RESTRICT')
 
-            table.increments('project_resource_id')
-            table.integer('project_id')
-                .notNullable()
-                .unsigned()
-                .references('project_id')
-                .inTable('projects')
-                .onDelete('RESTRICT')
-                .onUpdate('RESTRICT')
-            table.integer('resource_id')
-                .notNullable()
-                .unsigned()
-                .references('resource_id')
-                .inTable('resources')
-                .onDelete('RESTRICT')
-                .onUpdate('RESTRICT')
-        })
-}
+    })
 
-exports.down = async function(knex) {
+    .createTable('project_resources', table => {
 
-    await knex.schema
-        .dropTableIfExists('project_resources')
-        .dropTableIfExists('tasks')
-        .dropTableIfExists('resources')
-        .dropTableIfExists('projects')
+        table.increments('project_resources_id')
+        table.string('resource_assignment', 128)
+        table.integer('resource_id')
+        .notNullable()
+        .unsigned()
+        .references('resource_id')
+        .inTable('resource')
+        .onUpdate('RESTRICT')
+        .onDelete('RESTRICT')
 
-}
+    })
+  };
+  
+  exports.down = function(knex){
+
+    return knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    
+  };
